f973563cf3283a7adcff66c68068c2be3739a8a8
Mike FABIAN <mfabian@redhat.com>
2014-04-23 16:51:30 +0200

    Remove useless “$Id: $” from comments in the file headers
    This is not used by git, it was only useful for older version
    control systems.

--- engine/tabsqlitedb.py
+++ engine/tabsqlitedb.py
@@ -19,8 +19,6 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #
-# $Id: $
-#
 
 import sys
 if sys.version_info < (3,0,0):
